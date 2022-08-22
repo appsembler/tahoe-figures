7cc593472e00fca6022d037acc9b716b0f8dddee
Shadi Naif <shadinaif@gmail.com>
2022-08-22 15:45:33 +0300

    Fix Pypi Publish

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@
 
 from setuptools import setup
 
+description = 'Backends and customizations for Figures in Tahoe.'
+
 setup(
     name='tahoe-figures-plugins',
     version='0.1.1',
-    description='Backends and customizations for Figures in Tahoe.',
+    description=description,
+    long_description=description,
+    long_description_content_type="text/markdown",
     packages=[
         'tahoe_figures_plugins',
     ],
